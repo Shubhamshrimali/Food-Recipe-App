cbe077a7fec2ef9d22113a5662d6c28912503231
shubham shrimali <shubhamshrimali037@gmail.com>
2020-03-15 20:33:36 +0530

    Comit

---
+++ app/src/main/java/com/haerul/foodsapp/view/detail/DetailView.java
@@ -0,0 +1,11 @@
+
+package com.haerul.foodsapp.view.detail;
+
+import com.haerul.foodsapp.model.Meals;
+
+public interface DetailView {
+    void showLoading();
+    void hideLoading();
+    void setMeal(Meals.Meal meal);
+    void onErrorLoading(String message);
+}
